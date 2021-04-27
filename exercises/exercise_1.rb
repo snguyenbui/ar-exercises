c1ce599ff7e7ef1c3b54674242d8b6bc5bfd95b9
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-04-27 00:44:47 +0000

    adding employee data and created relations between employee and stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,9 +4,6 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-class Store < ActiveRecord::Base
-  
-end
 
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
